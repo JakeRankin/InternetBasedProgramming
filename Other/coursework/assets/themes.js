21e8e22c01d40b15682e44581f2fb07386295bba
1507420 <1507420@rgu.ac.uk>
2016-04-15 14:53:48 +0100

    15/04/16 - Upload

---
+++ Other/coursework/assets/themes.js
@@ -0,0 +1,7 @@
+function whitetheme() {
+    document.getElementById("theme").style.backgroundColor="white";  document.getElementById("theme").style.color="red";
+
+}
+function greytheme() {
+    document.getElementById("theme").style.backgroundColor="#4d4d4d"; document.getElementById("theme").style.color="lightsteelblue";
+}
